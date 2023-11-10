9eb54d7219baac77eed64355dda3e0856aff9060
JeremieG06 <91867175+JeremieG06@users.noreply.github.com>
2023-11-10 19:01:25 +0100

    update footer

--- galeries-photos-mf/src/components/footer/footer.js
+++ galeries-photos-mf/src/components/footer/footer.js
@@ -5,7 +5,7 @@ import './footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>Réalisation par Jérémie Garod</p>
+      <p>Réalisation par Jérémie Garod - web developpeur junior</p>
       <p>Galerie photos MF 2023 - Tous droits réservés</p>
     </footer>
   );
